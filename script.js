5b7004b5c5ade0c9a9ee6980d75ae256a393ea27
Diinya <156699160+Diinya@users.noreply.github.com>
2025-02-18 16:52:29 +0300

    multiple use of rotation

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
+let img = document.getElementById("image");
+let rotation = 0;
+
+
 function rotate() {
-    
-    document.getElementById("image").style.transform = "rotate(360deg)";
+    if (rotation >= 360) {
+        rotation = 0
+    }
+    else {
+        rotation += 360;
+    }
+
+    document.getElementById("image").style.transform = `rotate(${rotation}deg)`;
     document.getElementById("image").style.transitionDuration = "2s";
 }
+
+img.addEventListener("click", function(){rotate()});
+
+
